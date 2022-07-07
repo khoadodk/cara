409e917d516802513d080dd5cc65d5a35d684984
DESKTOP-NPJ9KD5\DK <khoado.dk@gmail.com>
2022-07-07 15:14:40 -0400

    Added single product and shop page

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Navbar Events
 const bar = document.querySelector('#bar');
 const nav = document.querySelector('#navbar');
 const close = document.querySelector('#close');
@@ -13,3 +14,20 @@ if (close) {
     nav.classList.remove('active');
   });
 }
+
+// Single product events
+const mainImg = document.querySelector('#mainImg');
+const smallImg = document.querySelectorAll('.small-img');
+
+smallImg[0].onclick = function () {
+  mainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = function () {
+  mainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = function () {
+  mainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = function () {
+  mainImg.src = smallImg[3].src;
+};
